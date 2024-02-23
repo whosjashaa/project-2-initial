d6cf60c158339b7b1aa0c983f76a691e22f77830
whosjashaa <josh.exe22@gmail.com>
2024-02-23 00:12:10 -0700

    Adding Project 2

--- src/main/java/com/csc205/project2/Project2.java
+++ src/main/java/com/csc205/project2/Project2.java
@@ -13,29 +13,120 @@ public class Project2 {
 
     public static void main(String[] args) {
 
-        /*
-        The following code works with the example design given.
-         */
+        interface ThreeDimensionalShape {
+            double surfaceArea(); // Method to calculate surface area of a shape
 
-        /*ThreeDimensionalShape sphere = new Sphere(2.0);
-        ThreeDimensionalShape cube = new Cube(5.0);
-        ThreeDimensionalShape cylinder = new Cylinder(4.0, 1.0);
+            double volume();      // Method to calculate volume of a shape
+        }
 
-        // We'll cover this stuff in module 4
-        List<ThreeDimensionalShape> shapes = new ArrayList<>();
-        shapes.add(sphere);
-        shapes.add(cube);
-        shapes.add(cylinder);
+// Abstract class representing a generic 3D shape
+        abstract class Shape implements ThreeDimensionalShape {
+            // Common methods or properties for 3D shapes can go here
+        }
 
-        shapes.forEach(System.out::println);*/
+// Sphere class
+        class Sphere extends Shape {
+            private double radius; // Radius of the sphere
 
-        /*
-        The following code works with the code given.
-         */
+            public Sphere(double radius) {
+                this.radius = radius;
+            }
 
-        Sphere sphere = new Sphere(2.0);
-        System.out.println(sphere);
+            public double getRadius() {
+                return radius;
+            }
+
+            @Override
+            public double surfaceArea() {
+                return 4 * Math.PI * radius * radius; // Formula for surface area of a sphere
+            }
+
+            @Override
+            public double volume() {
+                return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3); // Formula for volume of a sphere
+            }
+
+            @Override
+            public String toString() {
+                return String.format("Sphere {radius=%.2f, surface area=%.2f, volume=%.2f}", radius, surfaceArea(), volume());
+            }
+        }
+
+// Cube class
+        class Cube extends Shape {
+            private double width; // Width of the cube
+
+            public Cube(double width) {
+                this.width = width;
+            }
+
+            public double getWidth() {
+                return width;
+            }
+
+            @Override
+            public double surfaceArea() {
+                return 6 * width * width; // Formula for surface area of a cube
+            }
+
+            @Override
+            public double volume() {
+                return Math.pow(width, 3); // Formula for volume of a cube
+            }
+
+            @Override
+            public String toString() {
+                return String.format("Cube {width=%.2f, surface area=%.2f, volume=%.2f}", width, surfaceArea(), volume());
+            }
+        }
+
+// Cylinder class
+        class Cylinder extends Shape {
+            private double height; // Height of the cylinder
+            private double radius; // Radius of the cylinder
+
+            public Cylinder(double height, double radius) {
+                this.height = height;
+                this.radius = radius;
+            }
+
+            public double getHeight() {
+                return height;
+            }
+
+            public double getRadius() {
+                return radius;
+            }
+
+            @Override
+            public double surfaceArea() {
+                return 2 * Math.PI * radius * (radius + height); // Formula for surface area of a cylinder
+            }
+
+            @Override
+            public double volume() {
+                return Math.PI * Math.pow(radius, 2) * height; // Formula for volume of a cylinder
+            }
+
+            @Override
+            public String toString() {
+                return String.format("Cylinder {height=%.2f, radius=%.2f, surface area=%.2f, volume=%.2f}", height, radius, surfaceArea(), volume());
+            }
+        }
+
+// Driver class for testing
+        class Main {
+            public void main(String[] args) {
+                // Instantiate objects of different shapes
+                Sphere sphere = new Sphere(2.0);
+                Cube cube = new Cube(5.0);
+                Cylinder cylinder = new Cylinder(4.0, 1.0);
+
+                // Display details of each shape
+                System.out.println(sphere);
+                System.out.println(cube);
+                System.out.println(cylinder);
+            }
+        }
     }
-
 }
-
